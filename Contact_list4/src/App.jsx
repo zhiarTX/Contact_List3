6fba8a8500b7e25345c900df9f07e64cf2e53481
zhiarTX <138315070+zhiarTX@users.noreply.github.com>
2023-10-12 14:55:21 -0600

    commit 3
    adding more information

--- Contact_list4/src/App.jsx
+++ Contact_list4/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import "./App.css";
-
 import ContactList from "./assets/componets/ContactList";
+import SelectedContact from "./assets/componets/SelectedContact"
+
+
 export default function App () {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
   return (
     <>
+    {selectedContactId ? (
+      <div>Selected Contact View</div>
+    ) : (
         <ContactList />
+    )}
         </>
   );
 }
--- Contact_list4/src/assets/componets/ContactRow.jsx
+++ Contact_list4/src/assets/componets/ContactRow.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-export default function ContactRow({ contact }) {
+export default function ContactRow({ setSelectedContactId, contact }) {
     return(
-        <tr>
+        <tr
+        onClick={ () => {
+            setSelectedContactId (contact.id);
+        }}
+        >
         <td>{contact.name}</td>
         <td>{contact.email}</td>
         <td>{contact.phone}</td>
