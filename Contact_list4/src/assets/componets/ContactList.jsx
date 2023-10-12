7c66b68db656689d4e3049d7ba9706dfe261fef0
zhiarTX <138315070+zhiarTX@users.noreply.github.com>
2023-10-12 14:36:53 -0600

    2nd commit
    added information

---
+++ Contact_list4/src/assets/componets/ContactList.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import ContactRow from "./ContactRow";
+import { useEffect } from "react";
+
+const dummyContacts = [
+    { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+    { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+    { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+  ];
+export default function ContactList(){
+    const [contacts, setContacts] = useState (dummyContacts);
+   useEffect (() =>{
+    async function fetchContacts() {
+        try {
+            const response = await fetch(
+                "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
+            );
+            const result = await response.json()
+            setContacts(result);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    fetchContacts()
+   }, []);
+
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th colSpan="3">Contact List</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Name</td>
+                    <td>Email</td>
+                    <td>Phone</td>
+                </tr>
+                {contacts.map((contact) => {
+                    return <ContactRow key={contact.id} 
+                    contact={contact}/>;
+                })}
+            </tbody>
+        </table>
+    );
+}
---
+++ Contact_list4/src/assets/componets/ContactRow.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function ContactRow({ contact }) {
+    return(
+        <tr>
+        <td>{contact.name}</td>
+        <td>{contact.email}</td>
+        <td>{contact.phone}</td>
+        </tr>
+    );
+}
